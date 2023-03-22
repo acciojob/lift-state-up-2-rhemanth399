6e9d50a6565c45d7f60d5c35903251cb0f5e58d5
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-22 19:04:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
 import React from "react";
+import {useState} from 'react'
 import './../styles/App.css';
 
 const App = () => {
+  const [input,setInput]=useState('')
+  let handle=(e)=>{
+    setInput(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <p>{input}</p>
+
+        </div>
+        <div className="child">
+          <h1>Child Component</h1>
+          <input type="text" onChange={handle} value={input}/>
+        </div>
     </div>
   )
 }
